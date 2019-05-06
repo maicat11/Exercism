41575ef9ded009ced01722af23cff9b41382e70e
maicat11 <mvargas@lumatax.com>
2019-05-06 05:42:47 -0700

    add new problems

--- python/luhn/luhn.py
+++ python/luhn/luhn.py
@@ -1,6 +1,18 @@
 class Luhn(object):
     def __init__(self, card_num):
-        pass
+        self.card_num = card_num.replace(' ', '')[::-1]
 
     def is_valid(self):
-        pass
+        if len(self.card_num) <= 1 or not self.card_num.isdigit():
+            return False
+
+        card_sum = 0
+        for i, num in enumerate(list(map(int, self.card_num))):
+            if i % 2 == 0:
+                card_sum += num
+            else:
+                if num == 9:
+                    card_sum += 9
+                else:
+                    card_sum += (num*2) % 9
+        return card_sum % 10 == 0
--- python/minesweeper/minesweeper.py
+++ python/minesweeper/minesweeper.py
@@ -1,3 +1,35 @@
+def is_mine(grid, i, j):
+    return i >= 0 and i < len(grid) and j >= 0 and j < len(grid[i]) and grid[i][j] == '*'
+
+
+def mine_counter(grid, row, col):
+    counter = 0
+    for i in range(row - 1, row + 2):
+        for j in range(col - 1, col + 2):
+            if is_mine(grid, i, j):
+                counter += 1
+    return counter
+
+
 def board(input_board_array):
-    # Function body starts here
-    pass
+    if not input_board_array:
+        return input_board_array
+    output = []
+    row_length = len(input_board_array[0])
+    for row in range(len(input_board_array)):
+        if len(input_board_array[row]) != row_length:
+            raise ValueError('Row length mismatch.')
+        line_output = ''
+        for col in range(len(input_board_array[row])):
+            if input_board_array[row][col] == '*':
+                line_output += '*'
+            elif input_board_array[row][col] == ' ':
+                count = mine_counter(input_board_array, row, col)
+                if count == 0:
+                    line_output += ' '
+                else:
+                    line_output += str(count)
+            else:
+                raise ValueError('Invalid character.')
+        output.append(line_output)
+    return output
