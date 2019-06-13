05daa455f6c40ca0fd84efa7c316e2db29ee20d2
maicat11 <mvargas@lumatax.com>
2019-06-12 21:04:17 -0700

    additional python exercises

--- python/book-store/book_store.py
+++ python/book-store/book_store.py
@@ -1,2 +1,8 @@
 def total(basket):
-    pass
+    cost = 0
+    if len(set(basket)) <= 1:
+        cost = 800 * len(basket)
+        return cost
+
+    if len(set(basket)) == 2:
+        cost = (800)
