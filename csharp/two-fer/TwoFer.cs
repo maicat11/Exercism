8a1a7b6ceccefe402258d462cb2f7c1637c6bb1c
Mailei Vargas <maileivargas@gmail.com>
2019-09-21 11:01:00 -0700

    Test

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -7,5 +7,6 @@ public static class TwoFer
     public static string Speak()
     {
         throw new NotImplementedException("You need to implement this function.");
+        "This is a test"
     }
 }
