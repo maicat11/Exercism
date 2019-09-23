e7ea9dc5d75196882a86dcbdb5a4c64ededf955d
Mailei Vargas <maileivargas@gmail.com>
2019-09-23 15:14:18 -0700

    update exercises

---
+++ csharp/resistor-color/ResistorColor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class ResistorColor
+{
+    private static String[] color_map = new String[] {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
+
+    public static int ColorCode(string color)
+    {
+        return Array.IndexOf(color_map, color);
+    }
+    public static string[] Colors()
+    {
+        return color_map;
+    }
+}
+
+// String[] stringarr = new String[] {"Geeks", "GFG", "Noida"};
