544b243fdbb7f6fb0aec06cbceeaf8b165089f28
Mailei Vargas <maileivargas@gmail.com>
2019-09-23 09:16:24 -0700

    update leap and two-fer

--- csharp/two-fer/TwoFer.cs
+++ csharp/two-fer/TwoFer.cs
@@ -4,8 +4,8 @@ public static class TwoFer
 {
     // In order to get the tests running, first you need to make sure the Speak method 
     // can be called both without any arguments and also by passing one string argument.
-    public static string Speak()
+    public static string Speak(string name = "you")
     {
-        throw new NotImplementedException("You need to implement this function.");
+        return $"One for {name}, one for me.";
     }
 }
